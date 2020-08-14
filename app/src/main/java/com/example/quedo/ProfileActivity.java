0903779b18df370d120b8b2bf0868903278ddb17
DaScarlet <h.g.d.sandakalum@gmail.com>
2020-08-14 10:36:13 +0530

    frist upload

---
+++ app/src/main/java/com/example/quedo/ProfileActivity.java
@@ -0,0 +1,15 @@
+package com.example.quedo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+        getSupportActionBar().setTitle("Profile");
+    }
+}
